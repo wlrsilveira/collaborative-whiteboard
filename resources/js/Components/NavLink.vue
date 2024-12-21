31e1ff2cdf9f4d9a2a9961474695316d989d9efe
Wagner Silveira <wagner.resta@outlook.com>
2024-12-21 13:42:51 -0300

    Registration completion, log in, log out, but without wet api

---
+++ resources/js/Components/NavLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps<{
+    href: string;
+    active?: boolean;
+}>();
+
+const classes = computed(() =>
+    props.active
+        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-orange-600 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-orange-500 hover:text-orange-700 hover:border-orange-300 focus:outline-none focus:text-orange-700 focus:border-orange-300 transition duration-150 ease-in-out',
+);
+</script>
+
+<template>
+    <Link :href="href" :class="classes">
+        <slot />
+    </Link>
+</template>
---
+++ resources/js/Components/TextInput.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const model = defineModel<string>({ required: true });
+
+const input = ref<HTMLInputElement | null>(null);
+
+onMounted(() => {
+    if (input.value?.hasAttribute('autofocus')) {
+        input.value?.focus();
+    }
+});
+
+defineExpose({ focus: () => input.value?.focus() });
+</script>
+
+<template>
+    <input
+        class="rounded-md border-orange-300 shadow-sm focus:border-orange-500 focus:ring-orange-500"
+        v-model="model"
+        ref="input"
+    />
+</template>
